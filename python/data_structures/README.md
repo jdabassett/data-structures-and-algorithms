726b71ac83ffee47ecc7e2380c2f43ce04f2ae2c
jdabassett <jacobbassett@gmail.com>
2023-09-28 16:56:50 -0600

    mvp for code challenge 05

---
+++ python/data_structures/README.md
@@ -0,0 +1,31 @@
+# Data Structures:
+
+
+## Linked-List
+Data Structures: Linked-List
+
+#### Problem
+Write classes to create Node and Linked Lists instances.
+Create methods to insert, search if value exists and to return string representation of the linked list.
+
+[//]: # (#### Whiteboard Process)
+
+[//]: # (#### Code Example)
+
+#### Approach & Efficiency
+  * LinkedList.insert(value):
+    * Time Complexity: O(1)
+    * Space Complexity: O(1)
+  * LinkedList.includes(value):
+    * Time Complexity: O(n)
+    * Space Complexity: O(1)
+  * LinkedList.to_string()
+    * Time Complexity: O(n)
+    * Space Complexity: O(n)
+
+#### Solution
+Example
+```python
+```
+
+
--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,54 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
 
-    def __init__(self):
-        # initialization here
-        pass
+class Node():
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
 
-    def some_method(self):
-        # method body here
-        pass
+class LinkedList():
+    def __init__(self, head_node=None):
+        self.head = head_node
+
+    def insert(self, val):
+        try:
+            new_node = Node(val,None)
+            new_node.next=self.head
+            self.head = new_node
+        except Exception:
+            raise Exception(f"Exception: LinkedList.insert() unable to insert {val} intop linked list instance.")
+
+    def includes(self, val) -> bool:
+        try:
+            curr_node = self.head
+            while curr_node is not None:
+                if curr_node.value == val:
+                    return True
+                next_node = curr_node.next
+                curr_node = next_node
+            return False
+        except Exception:
+            raise Exception(f"Exception: LinkedList.includes() unable to verify that linked list instance includes {val}")
+
+    def __str__(self) -> str:
+        try:
+            list_val = []
+            curr_node = self.head
+
+            while curr_node is not None:
+                str_val = "{ "+str(curr_node.value)+" }"
+                list_val.append(str_val)
+                next_node = curr_node.next
+                curr_node = next_node
+
+
+            if curr_node is None:
+                list_val.append("NULL")
+
+            return " -> ".join(list_val) if len(list_val)>1 else "NULL"
+        except Exception:
+            raise Exception(f"Exception: LinkedList.to_string() unable to generate string representation of link list.")
+
+
+
 
 
 class TargetError:
