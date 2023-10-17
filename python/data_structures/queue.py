a7d45f7f43f3e4056d04ac61aae55e958348045a
jdabassett <jacobbassett@gmail.com>
2023-10-17 15:20:30 -0600

    finished with code challenge stack and queue

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,83 @@
+
+from data_structures.invalid_operation_error import InvalidOperationError
+
+class Node:
+    """
+    singly node class for creation of their instances
+    """
+    def __init__(self,value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     """
-    Put docstring here
+    queue class for the creation of their instances and methods for their maintenance
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, node = None):
+        self.front = node
+        self.back = self.front
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self,value):
+        """
+        queue method, create new nodes with value argument and add to back of queue
+        input: value
+        return: None
+        """
+        if self.front is None:
+            node_back = Node(value,None)
+            self.front = node_back
+            self.back = node_back
+        else:
+            node_back = Node(value, self.back)
+            self.back = node_back
+
+    def dequeue(self):
+        """
+        queue method, remove nodes from front of queue, return node value
+        input: None
+        return: value
+        """
+        if self.front is None:
+            raise InvalidOperationError("Cannot remove from an empty queue.")
+        elif self.front == self.back:
+            node_return = self.front
+            self.front = None
+            self.back = None
+            return node_return.value
+        else:
+            node_curr = self.back
+            node_prev = None
+            while node_curr.next:
+                node_prev = node_curr
+                node_curr = node_curr.next
+
+            self.front = node_prev
+            self.front.next = None
+
+            return node_curr.value
+
+
+    def peek(self):
+        """
+        queue method, checks value of node at front of queue, returns value
+        input: None
+        return: value
+        """
+        if self.front is None:
+            raise InvalidOperationError("Cannot peek from an empty queue.")
+        else:
+            node_return = self.front
+            return node_return.value
+
+
+    def is_empty(self):
+        """
+        queue method, checks if queue is empty
+        input: None
+        return: boolean
+        """
+        return self.front is None
+
+
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,24 +1,36 @@
 from data_structures.invalid_operation_error import InvalidOperationError
 
 class Node:
-    """"""
+    """
+    singly node class for the creation of node instances
+    """
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
 
 class Stack:
-    """"""
+    """
+    stack class for the creation of stack instances and methods for their maintenance
+    """
     def __init__(self, top: Node=None):
         self.top = top
 
     def push(self,value):
-        """"""
+        """
+        stack method, create new node with value and push onto top of stack
+        input: value
+        return: None
+        """
         node_top = Node(value)
         node_top.next = self.top
         self.top = node_top
 
     def pop(self):
-        """"""
+        """
+        stack method, remove node from top of stack, return node value
+        input: None
+        return: value
+        """
         if isinstance(self.top,Node):
             node_top = self.top.next
             node_return = self.top
@@ -28,7 +40,11 @@ class Stack:
             raise InvalidOperationError('Method not allowed on empty collection')
 
     def peek(self):
-        """"""
+        """
+        stack method, check value of node from top of stack, return value
+        input: None
+        return: value
+        """
         if isinstance(self.top,Node):
             return self.top.value
         else:
@@ -36,7 +52,11 @@ class Stack:
 
 
     def is_empty(self):
-        """"""
+        """
+        stack method, check if stack is empty
+        input: None
+        return: boolean
+        """
         if isinstance(self.top,Node):
             return False
         else:
