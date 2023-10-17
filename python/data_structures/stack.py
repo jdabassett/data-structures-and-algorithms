13492c7afe5f62e9d73ffa83e5203fe6cf422ef6
jdabassett <jacobbassett@gmail.com>
2023-10-17 13:24:31 -0600

    mvp with stack

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,44 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+class Node:
+    """"""
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class Stack:
-    """
-    Put docstring here
-    """
+    """"""
+    def __init__(self, top: Node=None):
+        self.top = top
 
-    def __init__(self):
-        # initialization here
-        pass
+    def push(self,value):
+        """"""
+        node_top = Node(value)
+        node_top.next = self.top
+        self.top = node_top
 
-    def some_method(self):
-        # method body here
-        pass
+    def pop(self):
+        """"""
+        if isinstance(self.top,Node):
+            node_top = self.top.next
+            node_return = self.top
+            self.top = node_top
+            return node_return.value
+        else:
+            raise InvalidOperationError('Method not allowed on empty collection')
+
+    def peek(self):
+        """"""
+        if isinstance(self.top,Node):
+            return self.top.value
+        else:
+            raise InvalidOperationError('Method not allowed on empty collection')
+
+
+    def is_empty(self):
+        """"""
+        if isinstance(self.top,Node):
+            return False
+        else:
+            return True
+
