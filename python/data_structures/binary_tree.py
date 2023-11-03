08be2c722e02adf14c216349f6f94e65a0686985
jdabassett <jacobbassett@gmail.com>
2023-11-02 21:30:21 -0600

    done for the day

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,11 +1,16 @@
 class BinaryTree:
     """
+    Class to make binary trees.
+    Attributes: root
     """
 
     def __init__(self, root=None):
         self.root = root
 
-    def pre_order(self):
+    def pre_order(self)->list:
+        """
+        For the pre-order traversal of binary tree.
+        """
         list_return = []
 
         def helper(curr_node):
@@ -18,7 +23,10 @@ class BinaryTree:
         helper(self.root)
         return list_return
 
-    def in_order(self):
+    def in_order(self)->list:
+        """
+        For the in-order traversal of binary tree.
+        """
         list_return = []
 
         def helper(curr_node):
@@ -31,7 +39,10 @@ class BinaryTree:
         helper(self.root)
         return list_return
 
-    def post_order(self):
+    def post_order(self)->list:
+        """
+        For the post-order traversal of binary tree.
+        """
         list_return = []
 
         def helper(curr_node):
@@ -46,6 +57,10 @@ class BinaryTree:
 
 
 class Node:
+    """
+    Class to make nodes for a binary tree.
+    Attributes: Value, Left(left node), Right(right node)
+    """
     def __init__(self, value=None, left=None, right=None):
         self.val = value
         self.left = left
@@ -56,4 +71,32 @@ class Node:
 
     def __str__(self):
         return f"{self.val}"
+
+    def __lt__(self,other):
+        if isinstance(other,Node):
+            if self.val <= other.val:
+                return True
+            else:
+                return False
+        else:
+            # TODO: handler exception
+
+    def __gt__(self, other):
+        if isinstance(other, Node):
+            if self.val >= other.val:
+                return True
+            else:
+                return False
+        else:
+            # TODO: handler exception
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            if self.val == other.val:
+                return True
+            else:
+                return False
+        else:
+            # TODO: handler exception
+
 
