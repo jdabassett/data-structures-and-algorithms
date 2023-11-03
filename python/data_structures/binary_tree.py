63ce36c610625af60ee8b4609f6ebbae504b4615
jdabassett <jacobbassett@gmail.com>
2023-11-02 19:05:20 -0600

    binary tree mvp

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,59 @@
 class BinaryTree:
     """
-    Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+        list_return = []
+
+        def helper(curr_node):
+            if curr_node is None:
+                return
+            list_return.append(curr_node.val)
+            helper(curr_node.left)
+            helper(curr_node.right)
+
+        helper(self.root)
+        return list_return
+
+    def in_order(self):
+        list_return = []
+
+        def helper(curr_node):
+            if curr_node is None:
+                return
+            helper(curr_node.left)
+            list_return.append(curr_node.val)
+            helper(curr_node.right)
+
+        helper(self.root)
+        return list_return
+
+    def post_order(self):
+        list_return = []
+
+        def helper(curr_node):
+            if curr_node is None:
+                return
+            helper(curr_node.left)
+            helper(curr_node.right)
+            list_return.append(curr_node.val)
+
+        helper(self.root)
+        return list_return
 
 
 class Node:
-    pass
+    def __init__(self, value=None, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"{self.val}"
+
+    def __str__(self):
+        return f"{self.val}"
+
